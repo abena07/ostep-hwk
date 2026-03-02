9d535f4dfc1f97802a3962fe99fe1aa299777663
Phillipa <pbennetteghan.work@gmail.com>
2026-03-02 00:45:17 +0000

    context switch

---
+++ ch6/context_switch.c
@@ -0,0 +1,22 @@
+// Measuring the cost of a context switch is a little trickier. The lmbench
+// benchmark does so by running two processes on a single CPU, and set-
+// ting up two UNIX pipes between them; a pipe is just one of many ways
+// processes in a UNIX system can communicate with one another. The first
+// process then issues a write to the first pipe, and waits for a read on the
+// second; upon seeing the first process waiting for something to read from
+// the second pipe, the OS puts the first process in the blocked state, and
+// switches to the other process, which reads from the first pipe and then
+// writes to the second. When the second process tries to read from the first
+// pipe again, it blocks, and thus the back-and-forth cycle of communication
+// continues. By measuring the cost of communicating like this repeatedly,
+// lmbench can make a good estimate of the cost of a context switch. You
+// can try to re-create something similar here, using pipes, or perhaps some
+// other communication mechanism such as UNIX sockets.
+// One difficulty in measuring context-switch cost arises in systems with
+// more than one CPU; what you need to do on such a system is ensure that
+// your context-switching processes are located on the same processor. For-
+// tunately, most operating systems have calls to bind a process to a partic-
+// ular processor; on Linux, for example, the sched setaffinity() call
+// is what you’re looking for. By ensuring both processes are on the same
+// processor, you are making sure to measure the cost of the OS stopping
+// one process and restoring another on the same CPU.
